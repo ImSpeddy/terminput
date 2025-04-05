cd99c3c2fbd7ec2d2302c799c17fc849635e631f
Ethan Mahlstedt <ethan.mahlstedt@hotmail.com>
2025-04-05 14:16:46 -0700

    Add TSDoc and JSDoc

--- index.ts
+++ index.ts
@@ -1,3 +1,17 @@
+/**
+ * Prompts user for input in the terminal and returns the input as a string.
+ * @async
+ * @param prompt - The prompt message to display to the user.
+ * @returns A promise that resolves to the user's input as a string.
+ * @example
+ * ```js
+ * const input = require('terminput')
+ * 
+ * const name = await input('What is your name? ')
+ * console.log(`Hello, ${name}!`)
+ * ```
+ */
+
 module.exports = async function (prompt: string) {
   if (prompt) {
     process.stdout.write(prompt);
